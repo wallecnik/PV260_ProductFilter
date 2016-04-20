908f2dc19955c9ddfc89d51f3e8a19252c45348b
tomas.valka <tomas.valka@mycroftmind.com>
2016-04-20 14:10:56 +0200

    init

---
+++ src/test/java/cz/muni/fi/pv260/productfilter/AtLeastNOfFilterTest.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pv260.productfilter;
+
+/**
+ * This filter passes if N or more of child filters pass (it does not matter how many fail)
+ */
+public class AtLeastNOfFilterTest {
+}
