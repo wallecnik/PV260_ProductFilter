ec9097669977fca3201fabe008f9dfd9fc0f1dfd
tomas.valka <tomas.valka@mycroftmind.com>
2016-04-20 14:14:20 +0200

    init

---
+++ src/test/java/cz/muni/fi/pv260/productfilter/ControllerTest.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pv260.productfilter;
+
+/**
+ * @author 422718
+ */
+public class ControllerTest {
+    
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
